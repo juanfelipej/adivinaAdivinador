b78f2d70e0433c6d94c6e1f2f2fec1a39f18fcfa
unknown <juanfelipej919@gmail.com>
2024-05-06 16:15:43 -0500

    prueba tutorial

---
+++ form.js
@@ -0,0 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwhN7rhMwCp341L7JDe2BvTvH3joBQR9QWGmrEohbOrLELHe-6DOgPgfCZDpBvBs9aHJA/exec';
+
+const form = document.forms['contact-form'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => {
+    if (response.ok) {
+      alert("Thank you! Your form is submitted successfully.");
+      window.location.href = '/confirmacion.html'; // Redirige a la página de confirmación
+    } else {
+      throw new Error('Network response was not ok.');
+    }
+  })
+  .catch(error => console.error('Error!', error.message));
+});
